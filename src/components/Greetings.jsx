d48814c09d231b99596d8c8fb5b92d3e53bb3ebb
kingjnr4 <62702613+kingjnr4@users.noreply.github.com>
2022-12-16 23:16:02 +0100

    components and rendering js

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,12 @@
+function Greeting() {
+  const name = 'faloke';
+  const time = 'evening';
+  return (
+    <>
+      <p>Hello {name}</p>
+      <p>Good {time}</p>
+    </>
+  );
+}
+
+export default Greeting;
